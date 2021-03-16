f865dcb05cf43f66b717424089d3580a40854c2c
vanessasot <vannexxaa@gmail.com>
2021-03-16 04:22:52 +0000

    Update User and Relationship models

--- db/migrate/20210315145135_create_relationships.rb
+++ db/migrate/20210315145135_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
       t.integer :follower_id
-      i.integer :followed_id
+      t.integer :followed_id
       
       t.timestamps null: false
     end
